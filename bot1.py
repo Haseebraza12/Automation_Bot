bfbfa170576b1f8f6e08007f6771db8aeea4e2fc
Haseebraza12 <munibhaseeb2@gmail.com>
2024-12-16 19:44:56 +0500

    Link 20 has been succees fully added

--- bot1.py
+++ bot1.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Sample form_data (this should be replaced with actual form data as required)
 form_data = {
