382f922292fa4d922e14ece15ad35a09f29f0c66
Haseebraza12 <munibhaseeb2@gmail.com>
2025-01-23 19:50:02 +0500

    fairburnga

--- fire.py
+++ fire.py
@@ -549,7 +549,6 @@ def handle_forsythcountyga_form(driver, url):
         driver.save_screenshot("error_main.png")
         return {"status": "Failed", "error": str(e)}
 
-
 def handle_fairburnga_form(driver, url):
     try:
         driver.get(url)
@@ -579,7 +578,7 @@ def handle_fairburnga_form(driver, url):
         details.send_keys(form_data["message"])
         print("Filled request details")
 
-        #checkbox
+        # Handle checkbox selection
         checkbox1_xpath = "/html/body/div[1]/div[2]/main/div/div[1]/form/div[2]/div/div[13]/div/div[1]/div/div/div/div/div/div/div"
         for checkbox_xpath in [checkbox1_xpath]:
             checkbox = wait.until(EC.element_to_be_clickable((By.XPATH, checkbox_xpath)))
@@ -594,8 +593,16 @@ def handle_fairburnga_form(driver, url):
         submit_button = wait.until(EC.element_to_be_clickable((By.XPATH, submit_xpath)))
         driver.execute_script("arguments[0].click();", submit_button)
         print("Clicked submit button")
-        time.sleep(5)
-        return {"status": "Success", "confirmation": "Form submitted"}
+        time.sleep(30)
+
+        try:
+            confirmation_message = driver.find_element(By.XPATH, "//*[contains(text(), 'Your security key is')]").text
+            security_key = re.search(r"Your security key is (\S+)", confirmation_message).group(1)
+            reference_number = re.search(r"Your request reference number is (\S+)", confirmation_message).group(1)
+            return {"status": "Success", "confirmation": "Form submitted", "reference_number": reference_number, "security_key": security_key}
+        except NoSuchElementException:
+            print("Confirmation message not found")
+            return {"status": "Success", "confirmation": "Form submitted", "reference_number": "", "security_key": ""}
 
     except Exception as e:
         print(f"Error in form handling: {str(e)}")
